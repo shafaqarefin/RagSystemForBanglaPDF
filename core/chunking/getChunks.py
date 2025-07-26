c96b9969ffba90efbb8d4a52354ef1d33261e600
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-07-26 21:03:02 +0600

    chunking function converting pdf text into chunks

--- core/chunking/getChunks.py
+++ core/chunking/getChunks.py
@@ -1,11 +1,10 @@
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
 def getChunks(text):
-    text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=500,
-        chunk_overlap=200,
-        length_function=len)
-    chunks = text_splitter.split_text(text)
-    return chunks
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=350,  # chunk size increases information each chunk contains
+        chunk_overlap=50,  # overlap decreases sentence info but increases context
+        separators=["\n\n", "\n", ".", "!", "?", ",", " ", "।", "|"]
+    )
+    return splitter.split_text(text)
